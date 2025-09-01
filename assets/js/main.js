5f034c51a268c0d3eeb0411926fff7b0d419e488
zouidine <med.zoui.dine@gmail.com>
2025-09-01 19:20:21 +0100

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,10 +20,10 @@
 
     if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      logo.src = "assets/img/logo/logo-2.svg";
+      logo.src = "assets/img/logo/moroccoai-logo.png";
     } else {
       header_navbar.classList.remove("sticky");
-      logo.src = "assets/img/logo/logo.svg";
+      logo.src = "assets/img/logo/moroccoai-logo.png";
     }
 
     // show or hide the back-top-top button
